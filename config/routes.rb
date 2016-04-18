0fbcb674fd9bb55177133b1a2d68e0f116a72ee7
shockstate <guillem.bonafonte@gmail.com>
2016-04-18 18:57:03 +0000

    push

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users
 
   get '/reply' => 'contributions#reply'
-  get 'contributions/discuss'
+  get '/discuss' => 'contributions#discuss'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
