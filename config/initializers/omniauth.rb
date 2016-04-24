5aaddac99cefb9cb1ff179eab83094c2de74d12a
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-04-24 15:03:45 +0200

    Revert "change credentials for Google Oauth"
    This reverts commit 38d460d68e28163e330c06f093356b461181d68f.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '348916042618-fkrhqnqpc429qa56jt14mtb111hgi0oc.apps.googleusercontent.com', 'rJS1uuc6gvVN--4FPVqz4u9N',
+  provider :google_oauth2, '348916042618-nr5mhrth6oag4ltdet8ogtnq7mih5gnh.apps.googleusercontent.com', 'RGoLmcBHSIopBzuNIezbU2Xp',
            {
                :name => "google"
            }
