3b34e578281f65f940e29752d5d9792e2f9ad2dc
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-02 20:32:06 +0200

    add user edition. closes #27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to action: 'paginap', id: @user.id }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
