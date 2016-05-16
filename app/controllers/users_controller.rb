b428405cd7a91ad41bcb94a9b13a3e5246ff6086
pere <pereberge@gmail.com>
2016-05-16 09:40:09 +0000

    threads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -89,7 +89,7 @@ class UsersController < ApplicationController
   
   def api_threads
     set_user
-    @contributions = @user.contributions
+    @contributions = @user.contributions.where(contr_type = 'comment' or contr_type = 'reply').all.order('CREATED_AT DESC')
     render json: @contributions
   end
 
